bd8d944c50db641fe55ba7109b555191e9c04fdd
Jimmy Smith <jasmith.188@gmail.com>
2020-05-12 22:42:52 -0400

    Updates

---
+++ precache-manifest.39e5ce80b619b9f8fc2ca87b27bc0a8b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d5b7e7bacc1da897671102893733ea",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "480598965de73d0f85a6",
+    "url": "/React_Portfolio/static/css/main.49cb9329.chunk.css"
+  },
+  {
+    "revision": "137609e1e1bb0af4920c",
+    "url": "/React_Portfolio/static/js/2.0d9e9862.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.0d9e9862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480598965de73d0f85a6",
+    "url": "/React_Portfolio/static/js/main.4f0d521e.chunk.js"
+  },
+  {
+    "revision": "2d93e3239f0577ba9343",
+    "url": "/React_Portfolio/static/js/runtime-main.e3c7ef78.js"
+  },
+  {
+    "revision": "c693abaf09860727cc519ba54def8020",
+    "url": "/React_Portfolio/static/media/biopicture.c693abaf.jpeg"
+  },
+  {
+    "revision": "31c8821f879919dbaae00753e89fae26",
+    "url": "/React_Portfolio/static/media/burger.31c8821f.jpg"
+  },
+  {
+    "revision": "25f51426ef58b690eb3feb7e1406b434",
+    "url": "/React_Portfolio/static/media/codebackground.25f51426.jpg"
+  },
+  {
+    "revision": "c8b8d87e5ef2b0328fd02f097c9f4e9d",
+    "url": "/React_Portfolio/static/media/hopsgrapes.c8b8d87e.jpg"
+  },
+  {
+    "revision": "23a2bfc6aa759c9f6d42b329d98e865d",
+    "url": "/React_Portfolio/static/media/infodosemain.23a2bfc6.jpg"
+  },
+  {
+    "revision": "9625c26c178ee7224c909dbe906dc13d",
+    "url": "/React_Portfolio/static/media/managementsystem.9625c26c.jpg"
+  },
+  {
+    "revision": "81edb0d41f2212ee46f3101c9d6349db",
+    "url": "/React_Portfolio/static/media/notetaker.81edb0d4.jpg"
+  },
+  {
+    "revision": "6311fd20340ef12204b7ac5870bfa0c5",
+    "url": "/React_Portfolio/static/media/password.6311fd20.jpg"
+  },
+  {
+    "revision": "383dca6d99efb1f94ef34f68fb7d5edb",
+    "url": "/React_Portfolio/static/media/plating.383dca6d.jpeg"
+  },
+  {
+    "revision": "76fce16b9edd8b30db9814912ad03eed",
+    "url": "/React_Portfolio/static/media/quiz.76fce16b.png"
+  },
+  {
+    "revision": "fdc3fd43f9e3babc8a99a23c5dc0b6c6",
+    "url": "/React_Portfolio/static/media/something.fdc3fd43.jpg"
+  },
+  {
+    "revision": "977bbfd501df40374ba8630d2572c9bb",
+    "url": "/React_Portfolio/static/media/workdayscheduler.977bbfd5.png"
+  }
+]);
