322d0e9dd380ec5637b554f8f105f717f0bfa40e
Jimmy Smith <jasmith.188@gmail.com>
2020-05-12 21:31:33 -0400

    adding contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Axios from 'axios';
 
 
 class Contact extends React.Component {
@@ -15,10 +18,76 @@ class Contact extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    this.setState({
+      disabled: true
+    });
+
+    Axios.post('http://localhost:3030/api/email', this.state)
+      .then(res => {
+        if (res.data.success) {
+          this.setState({
+            disabled: false,
+            emailSent: true
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          disabled: false,
+          emailSent: false
+        });
+      })
+
+  }
+
   render() {
     return (
       <div>
-        <Hero title={this.props.title} />
+        <div className="container-fluid">
+          <div className="contact-row">
+            <div className="col-md-8">
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                  <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label htmlFor="email">Email</Form.Label>
+                  <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label htmlFor="message">Full Name</Form.Label>
+                  <Form.Control id="message" name="message" type="textarea" value={this.state.message} onChange={this.handleChange} />
+                </Form.Group>
+                <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+                Send
+                </Button >
+                {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+
+              </Form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
