23e1921c44fa1e59429a843863ce399dc86a720c
Jimmy Smith <jasmith.188@gmail.com>
2020-06-05 21:56:48 -0400

    fixing routing

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ class App extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-        
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/React_Portfolio" component={Home} />
-              <Route exact path="/React_Portfolio/about" component={About} />
-              <Route path="/React_Portfolio/projects" component={Projects} />
-              <Route path="/React_Portfolio/contact" component={Contact} />
-              {/* <Route component={NoMatch} /> */}
-            </Switch>
-          </div>
-        </Router>
-      </React.Fragment>
+
+
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/React_Portfolio" component={Home} />
+            <Route exact path="/React_Portfolio/about" component={About} />
+            <Route path="/React_Portfolio/projects" component={Projects} />
+            <Route path="/React_Portfolio/contact" component={Contact} />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
+        </div>
+      </Router>
+
     );
   }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container"
 
 
 function Header() {
   return (
-   <Container>
-      <Navbar expand="lg" variant="light">
-        <Navbar.Brand href="/React_Portfolio">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item><Nav.Link href="/React_Portfolio/about">About Me</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/React_Portfolio/projects">Projects</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/React_Portfolio/contact">Contact Me</Nav.Link></Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      </Container>
+    <nav className="navbar">
+    <Link className="nav-link h1 name" to="/React_Portfolio">
+        <span id="nav">Jimmy Smith</span>
+    </Link>
+    {/* <!--Nav Items have been linked--> */}
+    <ul className="nav">
+        <li className="nav-item">
+            <Link className="nav-link" to="/React_Portfolio/about"><span id="nav">About</span></Link>
+        </li>
+        <li className="nav-item">
+            <Link className="nav-link" to="/React_Portfolio/projects"> <span  id="nav" >Projects</span></Link>
+        </li>
+        <li className="nav-item">
+            <Link className="nav-link" to="/React_Portfolio/contact"><span  id="nav">Contact</span></Link>
+        </li>
+
+    </ul>
+</nav>
    
   )
 } 
