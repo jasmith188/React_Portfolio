0d8bc37e19cfffbae80498bf10f4225508afd492
Jimmy Smith <jasmith.188@gmail.com>
2020-06-08 14:02:46 -0400

    cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel'
 import Image2 from "../images/burger.jpg"
 import Image3 from "../images/managementsystem.jpg"
 import Image4 from "../images/hopsgrapes.jpg"
-import Image5 from "../images/password.jpg"
+import Image6 from "../images/infodoselogin.jpg"
 import Image8 from "../images/workdayscheduler.png"
 
 
@@ -15,58 +15,59 @@ function Projects() {
             <Navbar />
             <Carousel>
                 <Carousel.Item>
-                    <img className="burger" src={Image2} alt={"Error"} height="350px" width="590px" />
-                    <Carousel.Caption>
+                <img className="burgerapp" src={Image2} alt={"Error"} height="450px" width="900px" />
+                <br />
+                    <Carousel.Caption />
+                        
+                       
                         <h3>Burger App</h3>
                         <p>This is a project that allows users to create and eat burgers.</p>
                         <a href="https://github.com/jasmith188/Burger"
                             className="github-app"> github.com/jasmith188/Burger
           </a>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="employee" src={Image3} alt={"Error"} height="350px" width="590px" />
+                    <img className="employee" src={Image3} alt={"Error"} height="450px" width="900px" />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption />
                         <h3>Employee Management App</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         <a href="https://github.com/jasmith188/Employee_Management_System"
                             className="github-app"> github.com/jasmith188/Employee_Management_System
           </a>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="hopsgrapes" src={Image4} alt={"Error"} height="350px" width="590px" />
-
-                    <Carousel.Caption>
+                    <img className="hopsgrapes" src={Image4} alt={"Error"} height="450px" width="900px" />
+                    <br />
+                    <Carousel.Caption />
                         <h3>Hops&Grapes App</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         <a href="https://github.com/jasmith188/Hops-Grapes"
                             className="github-app"> github.com/jasmith188/Hops-Grapes
           </a>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="infodose" src={Image5} alt={"Error"} height="350px" width="590px" />
-
-                    <Carousel.Caption>
+                    <img className="infodose" src={Image6} alt={"Error"} height="450px" width="900px" />
+                    <Carousel.Caption />
                         <h3>InfoDose App</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         <a href="https://lit-crag-01772.herokuapp.com"
                             className="github-app">lit-crag-01772.herokuapp.com
           </a>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="notetaker" src={Image8} alt={"Error"} height="350px" width="590px" />
+                    <img className="notetaker" src={Image8} alt={"Error"} height="450px" width="900px" />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption/>
                         <h3>Day Planner App</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         <a href="https://github.com/jasmith188/DayPlanner"
                             className="github-app"> github.com/jasmith188/DayPlanner
           </a>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
             </Carousel>
         </div>
