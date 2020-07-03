d5573f1c11740260d2f7995b207ab90e152f1776
Jimmy Smith <jasmith.188@gmail.com>
2020-07-03 12:24:48 -0400

    fixing mobile errors

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,7 @@ import Image3 from "../images/biopicture.jpeg"
 import Image2 from "../images/plating.jpeg"
 import Image from 'react-bootstrap/Image'
 import Image4 from "../images/badge.png"
+import Image5 from "../images/something.jpg"
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import { Row, Col, Container } from "react-bootstrap"
 
@@ -16,17 +17,24 @@ function About() {
             <br />
             <Container className="top-container">
                 <Row>
-                    <Col xs={6} >
+                    <Col xs={12} >
                         <div className="about-text">
                             Hello, my name is Jimmy Smith and I have recently recieved a Certificate in Full Stack Coding at the University of Central Florida. I have learned the fundamentals of different Languages and Applications used in front and back end development. I'm looking forward to contributing to my team's success by collaboratively creating different projects and applications.
                 </div>
                     </Col>
-                    <Col xs={6} >
+                </Row>
+                <Row>
+                    <Col xs={12} >
                         <Image src={Image3} rounded alt={"Error"} height="450px" width="390px" />
                     </Col>
                 </Row>
             </Container>
             <br />
+            <Row>
+                <Col className="something" xs={12} >
+                    <Image className="something" src={Image5} rounded alt={"Error"} height="500px" width="670px" />
+                </Col>
+            </Row>
             <Jumbotron>
                 <Container className="skills-container">
                     <h1 className="skills">Skills</h1>
@@ -34,8 +42,8 @@ function About() {
                         <Col xs={6} md={4} className="languages-column">
                             <h5>Languages</h5>
                             <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
                                 <li>Javascript</li>
                                 <li>Node.js</li>
                                 <li>SQL</li>
@@ -44,11 +52,12 @@ function About() {
                         <Col xs={6} md={4} className="mx-auto">
                             <h5>Applications</h5>
                             <ul>
+                                <li>React.js</li>
+                                <li>React Native</li>
                                 <li>MySQL</li>
                                 <li>MongoDB</li>
                                 <li>Express</li>
                                 <li>Handlebars</li>
-                                <li>React</li>
                                 <li>Mongoose</li>
                             </ul>
                         </Col>
@@ -57,20 +66,22 @@ function About() {
             </Jumbotron>
             <Container className="bottom-container">
                 <Row>
-                    <Col xs={4} >
+                    <Col xs={12} >
                         <div className="about-text">
                             Working in the hectic restaurant business for the past 16 years has honed my skills in teamwork, time-management and dealing with ambiguity.  These skills have allowed me to successfully transition my career from a chef to a Full Stack Developer.
                 </div>
                     </Col>
-                    <Col xs={4}>
+                </Row>
+                <Row>
+                    <Col xs={6}>
                         <Image src={Image2} rounded alt={"Error"} height="500px" width="270px" />
                     </Col>
-                    <Col xs={4}>
-                    <a
-                href="https://api.badgr.io/public/assertions/J4qMOUgvQyW5baLRTU7I2Q?identity__email=jasmith.188%40gmail.com"
-                className="badgr">
-                <Image src={Image4} rounded alt={"Error"} height="300px" width="300px" />
-            </a>
+                    <Col xs={6}>
+                        <a
+                            href="https://api.badgr.io/public/assertions/J4qMOUgvQyW5baLRTU7I2Q?identity__email=jasmith.188%40gmail.com"
+                            className="badgr">
+                            <Image src={Image4} rounded alt={"Error"} height="300px" width="300px" />
+                        </a>
                     </Col>
                 </Row>
             </Container>
