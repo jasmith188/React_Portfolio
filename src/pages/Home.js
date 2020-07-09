205f8265c95ad2c694c2e5ea8691345004aa05af
Jimmy Smith <jasmith.188@gmail.com>
2020-07-08 23:05:22 -0400

    updating resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Image from 'react-bootstrap/Image'
 import Image5 from "../images/something.jpg"
 import { Row, Col, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin, faGithub, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 function Home() {
     return (
@@ -13,43 +13,44 @@ function Home() {
             <Navbar />
             <Jumbotron fluid>
                 <Container>
-                <h1>Hello, I'm Jimmy Smith.</h1>
-                <p>
-                    I'm a full-stack web developer.
+                    <h1>Hello, I'm Jimmy Smith.</h1>
+                    <p>
+                        I'm a full-stack web developer.
                 </p>
                 </Container>
+                <Row>
+                    <Col xs={12} >
+                        <Image className="something" src={Image5} alt={"Error"} height="200px" width="370px" />
+                    </Col>
+                </Row>
             </Jumbotron>
 
-            <Jumbotron fluid>
+            <div>
                 <Container className="social-home">
-                
-            <a
-                href="https://github.com/jasmith188"
-                className="github social">
-                <FontAwesomeIcon icon={faGithub} size="3x" />
-            </a>
-            <a
-                href="https://www.linkedin.com/in/jamessmith812/"
-                className="linkedin social">
-                <FontAwesomeIcon icon={faLinkedin} size="3x" />
-            </a>
-            <a
-                href="https://twitter.com/jimmysmith812"
-                className="twitter social">
-                <FontAwesomeIcon icon={faTwitter} size="3x" />
-            </a>
-            <a
-                href="https://www.instagram.com/jimmysmith812/"
-                className="instagram social">
-                <FontAwesomeIcon icon={faInstagram} size="3x" />
-            </a>
+
+                    <a
+                        href="https://github.com/jasmith188"
+                        className="github social">
+                        <FontAwesomeIcon icon={faGithub} size="3x" />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/jamessmith812/"
+                        className="linkedin social">
+                        <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                    </a>
+                    <a
+                        href="https://twitter.com/jimmysmith812"
+                        className="twitter social">
+                        <FontAwesomeIcon icon={faTwitter} size="3x" />
+                    </a>
+                    <a
+                        href="https://www.instagram.com/jimmysmith812/"
+                        className="instagram social">
+                        <FontAwesomeIcon icon={faInstagram} size="3x" />
+                    </a>
                 </Container>
-            </Jumbotron>
-            <Row>
-                <Col xs={12} >
-                    <Image className="something" src={Image5} alt={"Error"} height="200px" width="370px" />
-                </Col>
-            </Row>
+            </div>
+
         </div>
     );
 }
