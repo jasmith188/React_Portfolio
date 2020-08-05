add9e92dc0bcbc489da7884657556ea38ba23f13
James Smith <jasmith.188@gmail.com>
2020-08-05 14:16:02 -0400

    Updates

---
+++ precache-manifest.b4f31d252dac4f3bc6cb99afcc7841d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5536e3699ef4087562800d692ca9890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "873b000e46dd335058c4",
+    "url": "/static/css/main.27cd9282.chunk.css"
+  },
+  {
+    "revision": "614e1b68e374fa327bc8",
+    "url": "/static/js/2.c5615b27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c5615b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873b000e46dd335058c4",
+    "url": "/static/js/main.557a9e0b.chunk.js"
+  },
+  {
+    "revision": "fd227d327c1e681f1a06",
+    "url": "/static/js/runtime-main.75ce7eaf.js"
+  },
+  {
+    "revision": "4d416419ac453ad733ee2f6b8b26ed37",
+    "url": "/static/media/badge.4d416419.png"
+  },
+  {
+    "revision": "c693abaf09860727cc519ba54def8020",
+    "url": "/static/media/biopicture.c693abaf.jpeg"
+  },
+  {
+    "revision": "31c8821f879919dbaae00753e89fae26",
+    "url": "/static/media/burger.31c8821f.jpg"
+  },
+  {
+    "revision": "25f51426ef58b690eb3feb7e1406b434",
+    "url": "/static/media/codebackground.25f51426.jpg"
+  },
+  {
+    "revision": "f5378735f3e6886350e78e3e118d96e2",
+    "url": "/static/media/infodoselogin.f5378735.jpg"
+  },
+  {
+    "revision": "520ca82a1623b0ad15df0a29ebcc30d7",
+    "url": "/static/media/keyboard.520ca82a.jpg"
+  },
+  {
+    "revision": "383dca6d99efb1f94ef34f68fb7d5edb",
+    "url": "/static/media/plating.383dca6d.jpeg"
+  },
+  {
+    "revision": "fdc3fd43f9e3babc8a99a23c5dc0b6c6",
+    "url": "/static/media/something.fdc3fd43.jpg"
+  },
+  {
+    "revision": "73e56f403f23d98359668b49a6090590",
+    "url": "/static/media/suntide.73e56f40.png"
+  },
+  {
+    "revision": "2ff19569178b947748ae9824b4dc281b",
+    "url": "/static/media/vacationmakers.2ff19569.png"
+  },
+  {
+    "revision": "977bbfd501df40374ba8630d2572c9bb",
+    "url": "/static/media/workdayscheduler.977bbfd5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_Portfolio/precache-manifest.a28e0520e90e4cc87990f349f32bd6d9.js"
+  "/precache-manifest.b4f31d252dac4f3bc6cb99afcc7841d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
