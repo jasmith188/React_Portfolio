f7d0efad73992ce993a51509126a1c1c1871b3c7
James Smith <jasmith.188@gmail.com>
2020-08-24 18:18:00 -0400

    fix nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,70 @@
 import React from 'react';
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
 import { Nav, Navbar } from 'react-bootstrap';
 
 function Header() {
-    return (
-        <>
-            <Navbar style={{ backgroundColor: "black" }} variant="dark" expand="lg" sticky="top" collapseOnSelect>
-                <Navbar.Brand>James Smith</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Link
-                            activeClass="active"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={1000}
-                            to="home"
-                            className="nav-name">
-                            Home</Link>
-                        <Link
-                            activeClass="active"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={1000}
-                            to="about"
-                            className="nav-about">
-                            About</Link>
-                        <Link
-                            activeClass="active"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={1000}
-                            to="projects"
-                            className="nav-projects">
-                            Projects</Link>
-                        <Link
-                            activeClass="active"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={1000}
-                            to="contact"
-                            className="nav-contact">
-                            Contact</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </>
-    )
+  return (
+    <>
+      <Navbar
+        style={{ backgroundColor: 'black' }}
+        variant="dark"
+        expand="lg"
+        sticky="top"
+        collapseOnSelect
+      >
+        <Navbar.Brand>James Smith</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to="home"
+              className="nav-name"
+            >
+              Home
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to="about"
+              className="nav-about"
+            >
+              About
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to="projects"
+              className="nav-projects"
+            >
+              Projects
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+              to="contact"
+              className="nav-contact"
+            >
+              Contact
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
 
-export default Header
+export default Header;
